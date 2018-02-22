182ce5459f19bfafa0fbbb4c1edfb1585f7ef9c5
WhyNot <WhyNot@WHYNOT-KOMPUTER>
2018-02-22 01:06:52 +0100

    Add project files.

---
+++ PT main/Form4.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PT_main
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.views_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.export_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(169, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 69);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // views_btn
+            // 
+            this.views_btn.Location = new System.Drawing.Point(38, 12);
+            this.views_btn.Name = "views_btn";
+            this.views_btn.Size = new System.Drawing.Size(108, 69);
+            this.views_btn.TabIndex = 7;
+            this.views_btn.Text = "Załaduj";
+            this.views_btn.UseVisualStyleBackColor = true;
+            this.views_btn.Click += new System.EventHandler(this.views_btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(900, 284);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(34, 96);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // export_btn
+            // 
+            this.export_btn.Location = new System.Drawing.Point(837, 12);
+            this.export_btn.Name = "export_btn";
+            this.export_btn.Size = new System.Drawing.Size(101, 41);
+            this.export_btn.TabIndex = 15;
+            this.export_btn.Text = "Eskportuj Dane";
+            this.export_btn.UseVisualStyleBackColor = true;
+            this.export_btn.Click += new System.EventHandler(this.export_btn_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 432);
+            this.Controls.Add(this.export_btn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.views_btn);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button views_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button export_btn;
+    }
+}
---
+++ PT main/login.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT_main
+{
+    public partial class login : Form
+    {
+        string user1 = "admin";
+        string user2 = "100357";
+        string user3 = "122022";
+        string user4 = "pw";
+        string pswd1 = "admin";
+        string pswd2 = "100357";
+        string pswd3 = "122022";
+        string pswd4 = "pw";
+
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            int caseSwitch = 0;
+            string username = textBox_User.Text;
+            string password = textBox_Pass.Text;
+            if ((username == user1) && (password == pswd1)) //admin
+                caseSwitch = 1;
+            if ((username == user2) && (password == pswd2)) //100357
+                caseSwitch = 2;
+            if ((username == user3) && (password == pswd3)) //122022
+                caseSwitch = 2;
+            if ((username == user4) && (password == pswd4)) //pw
+                caseSwitch = 1;
+
+            
+            switch (caseSwitch) // 1 - prowadzacy; 2 - student
+            {
+                case 1:
+                    Form1 frm1 = new Form1(username);
+                    frm1.updateEvent += new EventHandler(handleUpdateEvent);
+                    frm1.FormClosed += new FormClosedEventHandler(form_FormClosed);
+                    Visible = false;
+                    frm1.Show();
+                    break;
+
+                case 2:
+                    Form2 frm2 = new Form2(username);
+                    frm2.updateEvent += new EventHandler(handleUpdateEvent);
+                    frm2.FormClosed += new FormClosedEventHandler(form_FormClosed);
+                    Visible = false;
+                    frm2.Show();
+                    break;
+
+                default:
+                    System.Windows.Forms.MessageBox.Show("Nieprawidłowy login lub hasło");
+                    break;
+            }
+
+        }
+
+        void form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //użyc jednej z ponizszych linii
+
+            this.Visible = true;  //dla powracania do logowania po zamknieciu form1
+            //this.Close();           //dla zamykania okna logowania po zamknieciu form1
+        }
+
+
+        void handleUpdateEvent(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+    }
+}
